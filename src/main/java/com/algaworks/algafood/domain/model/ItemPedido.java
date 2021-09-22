7bc388275779cd8a849b42ca709a56c8326082af
Samuel-Acc <samuel.r.carvalho@accenture.com>
2021-09-21 21:22:01 -0300

    Desafio: criando as tabelas Pedido e Item Pedido e mapeando as entidades

---
+++ src/main/java/com/algaworks/algafood/domain/model/ItemPedido.java
@@ -0,0 +1,46 @@
+package com.algaworks.algafood.domain.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+@Entity
+public class ItemPedido {
+	
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private Long id;
+	
+	@Column(nullable = false)
+	private Integer quantidade;
+	
+	@Column(nullable = false)
+	private BigDecimal precoUnitario;
+	
+	@Column(nullable = false)
+	private BigDecimal precoTotal;
+	
+	private String observacao;
+	
+	@JoinColumn(nullable = false)
+	@ManyToOne
+	private Pedido pedido;
+	
+	@JoinColumn(nullable = false)
+	@ManyToOne
+	private Produto produto;
+
+}
