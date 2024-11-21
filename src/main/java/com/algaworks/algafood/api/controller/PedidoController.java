a24c4531ade023d3dd340167a8c99dcd77710e3e
Carvalho <samuel.r.carvalho@accenture.com>
2024-11-20 23:08:04 -0300

    Aula 13.6

--- src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
@@ -4,16 +4,13 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,9 +24,10 @@ import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.model.Pedido;
 import com.algaworks.algafood.domain.model.Usuario;
+import com.algaworks.algafood.domain.repository.PedidoRepository;
+import com.algaworks.algafood.domain.repository.filter.PedidoFilter;
 import com.algaworks.algafood.domain.service.EmissaoPedidoService;
-import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
-import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.algaworks.algafood.infrastructure.repository.spec.PedidoSpecs;
 
 @RequestMapping("/pedidos")
 @RestController
@@ -46,6 +44,9 @@ public class PedidoController {
 	
 	@Autowired
 	private PedidoInputDisassembler pedidoInputDisassembler; 
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
 	
 	
 	
@@ -80,10 +81,11 @@ public class PedidoController {
 	
 	
 	@GetMapping 
-	public List<PedidoResumoModel> listar() { 
-		List<Pedido> pedidos = emissaoPedido.listar();
+	public List<PedidoResumoModel> pesquisar(PedidoFilter filtro) { 
+		//List<Pedido> todosPedidos = emissaoPedido.listar();
+		List<Pedido> todosPedidos = pedidoRepository.findAll(PedidoSpecs.usandoFiltro(filtro));
 
-		return pedidoResumoModelAssembler.toCollectionModel(pedidos); 
+		return pedidoResumoModelAssembler.toCollectionModel(todosPedidos); 
 	}
 	 
 	
---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/spec/PedidoSpecs.java
@@ -0,0 +1,46 @@
+package com.algaworks.algafood.infrastructure.repository.spec;
+
+import java.util.ArrayList;
+
+import javax.persistence.criteria.Predicate;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.repository.filter.PedidoFilter;
+
+public class PedidoSpecs {
+	
+	public static Specification<Pedido> usandoFiltro(PedidoFilter filtro){
+		return (root, query, builder) -> {
+			//uso do fetch para evitar o problema do n+1 do jpa,
+			//pois a cada consulta de pedido são feitas consultas em restaurante, cozinha e cliente
+			root.fetch("restaurante").fetch("cozinha"); //um restaurante tem uma cozinha
+			root.fetch("cliente");
+			root.fetch("enderecoEntrega").fetch("cidade").fetch("estado");
+		
+			
+			var predicates = new ArrayList<Predicate>();
+			
+			if (filtro.getClienteId() != null) {
+				predicates.add(builder.equal(root.get("cliente"), filtro.getClienteId())); //constroi um predicate
+			}
+			
+			if (filtro.getRestauranteId() != null) {
+				predicates.add(builder.equal(root.get("restaurante"), filtro.getRestauranteId())); //constroi um predicate
+			}
+			
+			if (filtro.getDataCriacaoInicio() != null) {
+				predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoInicio()));
+			}
+			
+			if (filtro.getDataCriacaoFim() != null) {
+				predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoFim()));
+			}
+			
+			return builder.and(predicates.toArray(new Predicate[0]));
+		};
+		
+	}
+	
+}
