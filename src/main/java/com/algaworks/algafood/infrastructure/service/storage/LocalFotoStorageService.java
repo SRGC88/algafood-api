9dcda994eb29b0041227599ded2e96502c6e591f
Carvalho <samuel.r.carvalho@accenture.com>
2025-01-31 21:22:36 -0300

    Aula 14.8

---
+++ src/main/java/com/algaworks/algafood/infrastructure/service/storage/LocalFotoStorageService.java
@@ -0,0 +1,45 @@
+package com.algaworks.algafood.infrastructure.service.storage;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
+
+import com.algaworks.algafood.domain.service.FotoStorageService;
+
+	
+	@Service
+	public class LocalFotoStorageService implements FotoStorageService {
+			
+		@Value("${algafood.storage.local.diretorio-fotos}")
+		private Path diretoriosFoto;
+	
+		
+		
+		@Override
+		public void armazenar(NovaFoto novaFoto) {
+			try {
+				//Monta o caminho onde será armazenada a foto
+				Path arquivoPath = getArquivoPath(novaFoto.getNomeArquivo());
+				
+				// Copia os bytes do arquivo com o getInputStream para o caminho de armazenamento da foto
+				FileCopyUtils.copy(novaFoto.getInputStream(), Files.newOutputStream(arquivoPath));
+				
+			} catch (Exception e) {
+				throw new StorageException("Não foi possível armazenar o arquivo.", e);
+			}
+			
+			
+		}
+		
+		//Concatena o diretório da foto com o nome do arquivo
+		//Para formar o caminho completo onde a foto será armazenada
+		
+		private Path getArquivoPath(String nomeArquivo) {
+			return diretoriosFoto.resolve(Path.of(nomeArquivo));
+			
+		}
+
+}
