06c0d88c548affe8baea10b7ff23181db5911d66
Carvalho <samuel.r.carvalho@accenture.com>
2025-02-20 08:43:33 -0300

    Aula 14.23

--- src/main/java/com/algaworks/algafood/infrastructure/service/storage/S3FotoStorageService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/storage/S3FotoStorageService.java
@@ -5,8 +5,12 @@ import java.io.InputStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.algaworks.algafood.core.storage.StorageProperties;
 import com.algaworks.algafood.domain.service.FotoStorageService;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 
 
 @Service
@@ -14,6 +18,9 @@ public class S3FotoStorageService implements FotoStorageService {
 	
 	@Autowired
 	private AmazonS3 amazonS3;
+	
+	@Autowired
+	private StorageProperties storageProperties;
 
 	@Override
 	public InputStream recuperar(String nomeArquivo) {
@@ -23,8 +30,28 @@ public class S3FotoStorageService implements FotoStorageService {
 
 	@Override
 	public void armazenar(NovaFoto novaFoto) {
-		// TODO Auto-generated method stub
+		try {
+			String caminhoArquivo = getCaminhoArquivo(novaFoto.getNomeArquivo());
+			
+			var objectMetadata = new ObjectMetadata();
+			objectMetadata.setContentType(novaFoto.getContentType());
+			
+			var putObjectRequest = new PutObjectRequest(
+					storageProperties.getS3().getBucket(), 
+					caminhoArquivo, 
+					novaFoto.getInputStream(), 
+					objectMetadata)
+					.withCannedAcl(CannedAccessControlList.PublicRead); //definir permissao de leitura para o objeto
+			
+			amazonS3.putObject(putObjectRequest);
+		} catch(Exception e) {
+			throw new StorageException("Não foi possível enviar arquivo para a Amazon S3", e);
+		}
 		
+	}
+
+	private String getCaminhoArquivo(String nomeArquivo) {
+		return String.format("%s/%s", storageProperties.getS3().getDiretorioFotos(), nomeArquivo);
 	}
 
 	@Override
