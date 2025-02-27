914cb529ec9d02c43dd4e1e65fdf904d58bc6c58
Carvalho <samuel.r.carvalho@accenture.com>
2025-02-26 22:54:35 -0300

    Aula 14-26

--- src/main/java/com/algaworks/algafood/infrastructure/service/storage/S3FotoStorageService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/storage/S3FotoStorageService.java
@@ -1,10 +1,8 @@
 package com.algaworks.algafood.infrastructure.service.storage;
 
-import java.io.InputStream;
 import java.net.URL;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import com.algaworks.algafood.core.storage.StorageProperties;
 import com.algaworks.algafood.domain.service.FotoStorageService;
@@ -15,7 +13,7 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
 
-@Service
+//@Service
 public class S3FotoStorageService implements FotoStorageService {
 	
 	@Autowired
